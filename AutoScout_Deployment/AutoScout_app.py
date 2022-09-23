ff62938d76e1e70a4af682c7734be7538cc61b03
Mustafa TOLU <103928566+mustafa-tolu@users.noreply.github.com>
2022-09-23 18:10:22 +0300

    Update AutoScout_app.py

--- AutoScout_Deployment/AutoScout_app.py
+++ AutoScout_Deployment/AutoScout_app.py
@@ -36,13 +36,16 @@ model = pickle.load(open(filename, 'rb'))
 st.sidebar.markdown('## :racing_car: Car Specs to Predict Price :racing_car:')
 st.sidebar.write("\n\n")
 
-make_model = st.sidebar.selectbox("Model Selection", ("Audi A3", "Audi A1", "Opel Insignia", "Opel Astra", "Opel Corsa", "Renault Clio", "Renault Espace", "Renault Duster"))
-hp_kW = st.sidebar.number_input("Horse Power:",min_value=40, max_value=294, value=120, step=5)
-age = st.sidebar.number_input("Age:",min_value=0, max_value=3, value=0, step=1)
-km = st.sidebar.number_input("km:",min_value=0, max_value=317000, value=10000, step=5000)
-Gears = st.sidebar.number_input("Gears:",min_value=5, max_value=8, value=5, step=1)
-Gearing_Type = st.sidebar.radio("Gearing Type", ("Manual", "Automatic", "Semi-automatic"))
+form = st.form(key='my_form')
 
+make_model = form.sidebar.selectbox("Model Selection", ("Audi A3", "Audi A1", "Opel Insignia", "Opel Astra", "Opel Corsa", "Renault Clio", "Renault Espace", "Renault Duster"))
+hp_kW = form.sidebar.number_input("Horse Power:",min_value=40, max_value=294, value=120, step=5)
+age = form.sidebar.number_input("Age:",min_value=0, max_value=3, value=0, step=1)
+km = form.sidebar.number_input("km:",min_value=0, max_value=317000, value=10000, step=5000)
+Gears = form.sidebar.number_input("Gears:",min_value=5, max_value=8, value=5, step=1)
+Gearing_Type = form.sidebar.radio("Gearing Type", ("Manual", "Automatic", "Semi-automatic"))
+
+submit_button = form.form_submit_button(label='Predict')
 
 my_dict = {"make_model":make_model, "hp_kW":hp_kW, "age":age, "km":km, "Gears":Gears, "Gearing_Type":Gearing_Type}
 df = pd.DataFrame.from_dict([my_dict])
@@ -63,7 +66,7 @@ st.table(df_show)
 
 # --------------------------
 
-if st.button("Predict"):
+if submit_button:
     pred = model.predict(df)
     col4, col5, col6, col7 = st.columns([3,1,2,2])
     col4.write("The estimated value of car price is € ")
